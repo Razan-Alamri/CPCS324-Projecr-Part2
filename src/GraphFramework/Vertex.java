387126bd08381104b719501d42bb6e2f8838c941
Razan Alamri <rmohammedalamri0001@stu.kau.edu.sa>
2023-05-30 03:55:22 +0300

    up

--- src/GraphFramework/Vertex.java
+++ src/GraphFramework/Vertex.java
@@ -1,5 +1,42 @@
 package GraphFramework;
 
 public class Vertex {
+    /**
+     * label of vertex
+     */
+    char label;
+    /**
+     * boolean to determine if the vertex is visited or not
+     */
+    boolean isVisited;
+    /**
+     * to indicate the position of vertex
+     */
+    int position;
+
+    /**
+     * empty constructor
+     */
+    public Vertex() {
+
+    }
+
+    /**
+     * 
+     * @param position of a vertex
+     */
+    public Vertex(int position) {
+        this.position = position;
+    }
+
+    /**
+     * 
+     * method to get position of vertex
+     * 
+     * @return the position number
+     */
+    public int getVertPos() {
+        return position;
+    }
 
 }
